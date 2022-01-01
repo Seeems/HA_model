6c1fcecde600a16b68b0a4f679d67084ec534209
Seeems <nicolai.mickel@gmail.com>
2022-01-01 22:43:28 +0100

    added functions for identifying the 4 ideal func. Needed to compare by test through each specific x value

--- graph.py
+++ graph.py
@@ -1,5 +1,29 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 import seaborn as sns
 
 import data_manager
+
+def plot_data(df):
+    i = 0
+    x = 0
+    v = 1
+
+
+    while x <= 9:
+        fig, axs = plt.subplots(nrows=5, ncols=1)
+        while i <= 4:
+            if v < 51:
+                axs[i].plot(df['x'], df[f'y{v}'])
+                axs[i].set_title(f'y{v}')
+
+            i += 1
+            v += 1
+
+        plt.draw()
+        plt.show()
+        x += 1
+        i = 0
+
+
--- model.py
+++ model.py
@@ -1,50 +1,71 @@
 from sklearn.metrics import mean_squared_error
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
+from scipy.fftpack import fft
+from IPython.display import display, Math
+from scipy.interpolate import interp1d
+
+def differentiate_functions(df1, df2):
+    best_func = []
+    error_list = []
+    error_dict = {}
+    df1_iterator = 1
+    while df1_iterator <= (len(df1.columns) - 2):
+
+        df2_iterator = 1
+
+        while df2_iterator <= (len(df2.columns) - 2):
+
+            error_dict[f'{df2_iterator}'] = np.sum(
+                (df1[f'y{df1_iterator}'] - df2[f'y{df2_iterator}']) ** 2)
+
+            df2_iterator += 1
+
+        best_ideal = {k: v for k, v in sorted(error_dict.items(), key=lambda item: item[1])}
+        best_func.append(list(best_ideal.keys())[0])
 
 
-def func(data):
+        df1_iterator += 1
+
+
+    return best_func
+
+def validate_best_func(df1, df2, best_func):
+    error_list = []
+    for i in best_func:
+        error_list.append(mean_squared_error(df1['y'], df2[f'y{i}']))
+
+    for i in error_list:
+        if i > np.sqrt(2):
+            print('Failed!')
+    return error_list
+
+def identify_ideal_func(data):
     ideal_df = data['ideal']
     test_df = data['test']
     train_df = data['train']
 
-    error_dict = {}
-    funciton_dict = {}
-
-    ideal_iterator = 1
-    lse = 0
-    lse_dict = {}
-    while ideal_iterator <= (len(ideal_df.columns)-2):
-
-        train_iterator = 1
-
-        while train_iterator <= (len(train_df.columns)-2):
-            print((len(train_df.columns)-2))
-            for i in train_df[f'y{train_iterator}']:
-                for j in ideal_df[f'y{ideal_iterator}']:
-                    lse += (j - i)**2
-
-            lse_dict[f'y{ideal_iterator} & y{train_iterator}'] = lse
-            error = mean_squared_error(train_df[f'y{train_iterator}'], ideal_df[f'y{ideal_iterator}'])
-            print(f'train: y{train_iterator} and ideal: y{ideal_iterator} = {error}')
-            error_dict[f'train: y{train_iterator} and ideal: y{ideal_iterator}'] = error
-            funciton_dict[f'y{ideal_iterator}'] = error
-            train_iterator += 1
-        ideal_iterator += 1
-
-    sorted_dict = {k: v for k, v in sorted(funciton_dict.items(), key=lambda item: item[1])}
-    sorted_lse_dict = {k: v for k, v in sorted(lse_dict.items(), key=lambda item: item[1])}
-    print(sorted_dict)
-        #myline = numpy.linspace(1, 22, 100)
-
-        # plt.scatter(x, y)
-        #plt.plot(myline, mymodel(myline))
-        #plt.show()
 
 
+    best_func = differentiate_functions(train_df, ideal_df)
 
-    #     lr.fit(, )
-    #        print("Training Set R² Score: {:.2f}".format(lr.score(train_df[['x']], train_df[[f'y{y}']])))
-    #
-    #  print("Test Set R² Score: {:.2f}".format(lrs[i].score(test_df[['x']], test_df[['y']])))
+
+    ll = [1,2,3,4]
+
+    for i in best_func:
+        plt.plot(ideal_df[f'x'], ideal_df[f'y{i}'], label = f"ideal y{i}")
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+    plt.legend()
+    plt.show()
+
+    for i in ll:
+        plt.plot(train_df[f'x'], train_df[f'y{i}'], label=f"train y{i}")
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+    plt.legend()
+    plt.show()
+
+    print(validate_best_func(test_df, ideal_df, best_func))
 
