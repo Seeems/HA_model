e0a7a320c7ce960bb3f9f70f6790211aea1b898e
Seeems <nicolai.mickel@gmail.com>
2023-01-07 12:49:59 +0100

    documentation of data_manager.py finished.

--- data_manager.py
+++ data_manager.py
@@ -1,23 +1,21 @@
 import os
 import json
-from typing import Dict, Any, Union, Iterator
-
+import setup_logger
+from exceptions import *
+from typing import Dict, Any, Union
 import sqlalchemy
 import sqlalchemy_utils
-from sqlalchemy.ext.declarative import declarative_base
 import pandas as pd
-
-import setup_logger
 
 # --------- GLOBAL VARIABLES ----------------
 # Project loaction for a variable execution location
+# Data tables of database in order to prevent SQL injection
 PROJECT_LOCATION = os.path.dirname(os.path.abspath(__file__))
 
 # --------- GLOBAL SETTINGS  ----------------
 # load config file
 with open(os.path.join(PROJECT_LOCATION, 'config.json'), 'r') as file:
-    config = json.load(file)
-
+    CONFIG = json.load(file)
 
 # Class Manager for whole data management. CSV and Database operations
 class Manager(object):
@@ -39,23 +37,23 @@ class Manager(object):
 
     def __init__(self):
         """
-        Initialize class with needed variables
+        Initialize class with needed variables and create connection
 
         :var self.db_url: Takes URI to database out of the config
         :var self.logger: creates a new logger from setup_logger.py
         :var self.db: SQL Alchemy engine (SQLite3)
         :var self.meta: create metadata object of SQL Alchemy
+
         :exec self.initialize_database
         """
         self.db_url = 'sqlite:///{}'.format(
             os.path.join(os.path.dirname(__file__),
-                         config['SQLALCHEMY_DATABASE_URI']))
+                         CONFIG['SQLALCHEMY_DATABASE_URI']))
         self.logger = setup_logger.setup('data_manager')
         self.db = sqlalchemy.create_engine(self.db_url)
         self.initialize_database()
         self.meta = sqlalchemy.MetaData()
         self.meta.reflect(bind=self.db)
-
 
     def initialize_database(self, directory_path: str = PROJECT_LOCATION) -> None:
         """
@@ -90,17 +88,38 @@ class Manager(object):
                                 self.db,
                                 if_exists='replace',
                                 index=True)
-                self.logger.info(f'{name} creation successfully!')
+                self.logger.info(f'{name} table creation successfully')
 
             else:
-                self.logger.info(f'{name} already exists!')
+                self.logger.info(f'{name} table already exists')
 
     def load_data(self) -> Dict[Any, Union[str, pd.DataFrame]]:
-        """Loads data from sqlite database into DataFrames"""
+        """
+        Loads data from sqlite database into DataFrames.
+
+        :param None
+
+        :return df_dict: dict[Any, Union[str, pd.DataFrame], contains data of all tables
+        """
+
+        # Initialize needed table names and dictionary
         table_list = self.meta.tables.keys()
         df_dict = {}
-        for table in table_list:
-            print(table)
-            df_dict[table] = pd.read_sql_query(f'SELECT * FROM {table}', self.db)
-        return df_dict
+        tables = CONFIG['TABLES']
 
+        for table in table_list:
+
+            # Check if right tables are called in order to prevent SQL Injections
+            # Handle exception if table do not exist
+            try:
+                if table in CONFIG['TABLES']:
+                    query = f'SELECT * FROM {table}'
+                    df_dict[table] = pd.read_sql_query(query, self.db)
+                else:
+                    raise MissingTableException
+            except MissingTableException:
+                self.logger.warning(f'{table} do not exists. Stopping program...')
+                exit(1)
+
+
+        return df_dict
---
+++ exceptions.py
@@ -0,0 +1,4 @@
+
+class MissingTableException(Exception):
+    """Raised when not existing or wrong table is called"""
+    pass
